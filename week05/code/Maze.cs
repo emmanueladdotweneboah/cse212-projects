0120dbff4affc6ccb84b89c701f3aadf6e212461
emmanueladdotweneboah <slimaddo06@gmail.com>
2026-02-12 19:40:19 +0000

    new work

--- week05/code/Maze.cs
+++ week05/code/Maze.cs
@@ -1,71 +1,33 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
-namespace Week05
+public static class Maze
 {
-    public static class Maze
+    public static void SolveMaze(
+        int[] maze,
+        int size,
+        int x,
+        int y,
+        List<(int x, int y)> currPath,
+        List<string> results)
     {
-        public static void SolveMaze(
-            int[] maze,
-            int size,
-            int x,
-            int y,
-            List<(int x, int y)> currPath,
-            List<string> results)
+        if (!IsValidMove(maze, size, x, y, currPath))
+            return;
+
+        currPath.Add((x, y));
+
+        if (IsEnd(maze, size, x, y))
         {
-            if (!IsValidMove(maze, size, x, y, currPath))
-                return;
-
-            currPath.Add((x, y));
-
-            if (IsEnd(maze, size, x, y))
-            {
-                results.Add(PathAsString(currPath));
-            }
-            else
-            {
-                SolveMaze(maze, size, x + 1, y, currPath, results); // Right
-                SolveMaze(maze, size, x - 1, y, currPath, results); // Left
-                SolveMaze(maze, size, x, y + 1, currPath, results); // Down
-                SolveMaze(maze, size, x, y - 1, currPath, results); // Up
-            }
-
-            // Backtrack
-            currPath.RemoveAt(currPath.Count - 1);
+            results.Add(currPath.AsString());
+        }
+        else
+        {
+            SolveMaze(maze, size, x + 1, y, currPath, results);
+            SolveMaze(maze, size, x - 1, y, currPath, results);
+            SolveMaze(maze, size, x, y + 1, currPath, results);
+            SolveMaze(maze, size, x, y - 1, currPath, results);
         }
 
-        // ------------------------------------------------------------
-        // Helper Methods (Already Provided in Assignment)
-        // ------------------------------------------------------------
-        private static bool IsEnd(int[] maze, int size, int x, int y)
-        {
-            return maze[y * size + x] == 2;
-        }
-
-        private static bool IsValidMove(
-            int[] maze,
-            int size,
-            int x,
-            int y,
-            List<(int x, int y)> path)
-        {
-            if (x < 0 || y < 0 || x >= size || y >= size)
-                return false;
-
-            if (maze[y * size + x] == 0)
-                return false;
-
-            if (path.Contains((x, y)))
-                return false;
-
-            return true;
-        }
-
-        private static string PathAsString(List<(int x, int y)> path)
-        {
-            return string.Join(" -> ",
-                path.Select(p => $"({p.x},{p.y})"));
-        }
+        currPath.RemoveAt(currPath.Count - 1);
     }
 }
--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -1,91 +1,88 @@
 using System;
 using System.Collections.Generic;
 
-namespace Week05
+public static class Recursion
 {
-    public static class Recursion
+    // ------------------------------------------------------------
+    // Problem 1: Recursive Squares Sum
+    // ------------------------------------------------------------
+    public static int SumSquaresRecursive(int n)
     {
-        // ------------------------------------------------------------
-        // Problem 1: Recursive Squares Sum
-        // ------------------------------------------------------------
-        public static int SumSquaresRecursive(int n)
-        {
-            if (n <= 0)
-                return 0;
+        if (n <= 0)
+            return 0;
 
-            return n * n + SumSquaresRecursive(n - 1);
+        return n * n + SumSquaresRecursive(n - 1);
+    }
+
+    // ------------------------------------------------------------
+    // Problem 2: Permutations Choose
+    // ------------------------------------------------------------
+    public static List<string> PermutationsChoose(char[] letters, int size)
+    {
+        var results = new List<string>();
+        PermuteHelper(letters, size, "", results);
+        return results;
+    }
+
+    private static void PermuteHelper(char[] letters, int size, string current, List<string> results)
+    {
+        if (current.Length == size)
+        {
+            results.Add(current);
+            return;
         }
 
-        // ------------------------------------------------------------
-        // Problem 2: Permutations Choose
-        // ------------------------------------------------------------
-        public static List<string> PermutationsChoose(char[] letters, int size)
+        foreach (char c in letters)
         {
-            var results = new List<string>();
-            PermuteHelper(letters, size, "", results);
-            return results;
-        }
-
-        private static void PermuteHelper(char[] letters, int size, string current, List<string> results)
-        {
-            if (current.Length == size)
+            if (!current.Contains(c))
             {
-                results.Add(current);
-                return;
-            }
-
-            foreach (char c in letters)
-            {
-                if (!current.Contains(c))
-                {
-                    PermuteHelper(letters, size, current + c, results);
-                }
+                PermuteHelper(letters, size, current + c, results);
             }
         }
+    }
 
-        // ------------------------------------------------------------
-        // Problem 3: Climbing Stairs (Memoization)
-        // ------------------------------------------------------------
-        public static int CountWaysToClimb(int s, Dictionary<int, int> remember)
+    // ------------------------------------------------------------
+    // Problem 3: Climbing Stairs (Memoization)
+    // ------------------------------------------------------------
+    public static int CountWaysToClimb(int s, Dictionary<int, int> remember)
+    {
+        if (s < 0)
+            return 0;
+
+        if (s == 0)
+            return 1;
+
+        if (remember.ContainsKey(s))
+            return remember[s];
+
+        int ways =
+            CountWaysToClimb(s - 1, remember) +
+            CountWaysToClimb(s - 2, remember) +
+            CountWaysToClimb(s - 3, remember);
+
+        remember[s] = ways;
+        return ways;
+    }
+
+    // ------------------------------------------------------------
+    // Problem 4: Wildcard Binary Patterns
+    // ------------------------------------------------------------
+    public static void ExpandBinaryPattern(string pattern, List<string> results)
+    {
+        int index = pattern.IndexOf('*');
+
+        if (index == -1)
         {
-            if (s < 0)
-                return 0;
-
-            if (s == 0)
-                return 1;
-
-            if (remember.ContainsKey(s))
-                return remember[s];
-
-            int ways =
-                CountWaysToClimb(s - 1, remember) +
-                CountWaysToClimb(s - 2, remember) +
-                CountWaysToClimb(s - 3, remember);
-
-            remember[s] = ways;
-            return ways;
+            results.Add(pattern);
+            return;
         }
 
-        // ------------------------------------------------------------
-        // Problem 4: Wildcard Binary Patterns
-        // ------------------------------------------------------------
-        public static void ExpandBinaryPattern(string pattern, List<string> results)
-        {
-            int index = pattern.IndexOf('*');
+        ExpandBinaryPattern(
+            pattern.Substring(0, index) + "0" + pattern.Substring(index + 1),
+            results);
 
-            if (index == -1)
-            {
-                results.Add(pattern);
-                return;
-            }
-
-            ExpandBinaryPattern(
-                pattern.Substring(0, index) + "0" + pattern.Substring(index + 1),
-                results);
-
-            ExpandBinaryPattern(
-                pattern.Substring(0, index) + "1" + pattern.Substring(index + 1),
-                results);
-        }
+        ExpandBinaryPattern(
+            pattern.Substring(0, index) + "1" + pattern.Substring(index + 1),
+            results);
     }
 }
